11ed2477b982d8fd7967a16b0c6c563f3d8df06b
Maz <mazspm@icloud.com>
2024-04-16 15:30:35 -0500

    adds auth

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,19 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export default function Nav() {
   return (
     <header className="sticky z-50 flex items-center justify-center">
       {/* <nav className="supports-[backdrop-filter] absolute w-full rounded-none bg-zinc-950/50 shadow-lg backdrop-blur-[0.5rem] transition-all duration-200 sm:top-4 sm:w-[36rem] sm:rounded-full"> */}
       <div className="flex h-14 w-full items-center justify-between border-b p-4 text-xl">
         <div>Garo</div>
-        <div>Sign in</div>
+        <div>
+          <SignedOut>
+            <SignInButton></SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton></UserButton>
+          </SignedIn>
+        </div>
       </div>
       {/* </nav> */}
     </header>
