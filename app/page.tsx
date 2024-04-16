c324bf45b2bfd33eae9b2aa0fb1d060bbb088c6c
Maz <mazspm@icloud.com>
2024-04-16 15:45:12 -0500

    adds auth for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import { db } from "@/server/db";
+import Images from "@/components/Images";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images].map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">Please sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
---
+++ components/Images.tsx
@@ -0,0 +1,17 @@
+import { db } from "@/server/db";
+
+export default async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images].map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
