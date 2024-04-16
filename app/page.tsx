c3cbbd3c458a1f33339745cb1cca517d02760ba7
Maz <mazspm@icloud.com>
2024-04-15 19:44:00 -0500

    Add mock images to the homepage and update global CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/75d5b4c5-5f2a-464b-aded-a3a62fec8e25-21ovs.jpeg",
+  "https://utfs.io/f/75d5b4c5-5f2a-464b-aded-a3a62fec8e25-21ovs.jpeg",
+  "https://utfs.io/f/75d5b4c5-5f2a-464b-aded-a3a62fec8e25-21ovs.jpeg",
+  "https://utfs.io/f/75d5b4c5-5f2a-464b-aded-a3a62fec8e25-21ovs.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <div key={image.id + 1} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       Hello (Garo in progress)
     </main>
   );
