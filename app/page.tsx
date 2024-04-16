a7920fbec3b8875ac04d7d9721f1aa1614bd071f
Maz <mazspm@icloud.com>
2024-04-15 20:48:10 -0500

    Add database query to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "@/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/75d5b4c5-5f2a-464b-aded-a3a62fec8e25-21ovs.jpeg",
   "https://utfs.io/f/75d5b4c5-5f2a-464b-aded-a3a62fec8e25-21ovs.jpeg",
@@ -10,12 +12,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages].map((image) => (
-          <div key={image.id + 1} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages].map((image, index) => (
+          <div key={index.toString() + image.id.toString()} className="w-48">
             <img src={image.url} />
           </div>
         ))}
