5c6256acd74331e5ee234e4910af0733662c6a14
Maz <mazspm@icloud.com>
2024-04-16 18:40:44 -0500

    sets up uploadthing

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,30 @@
+import { db } from "@/server/db";
+import { images } from "@/server/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const fileRoute = createUploadthing();
+
+export const uploadThingFileRouter = {
+  imageUploader: fileRoute({ image: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
+      const user = auth();
+
+      if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      return { userId: user.userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+
+      await db.insert(images).values({
+        name: file.name,
+        url: file.url,
+      });
+
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type uploadThingFileRouter = typeof uploadThingFileRouter;
---
+++ utils/uploadthing.ts
@@ -0,0 +1,9 @@
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+} from "@uploadthing/react";
+
+import type { uploadThingFileRouter } from "@/app/api/uploadthing/core";
+
+export const UploadButton = generateUploadButton<uploadThingFileRouter>();
+export const UploadDropzone = generateUploadDropzone<uploadThingFileRouter>();
