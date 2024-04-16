e6695540ed8fb7e49241eb40bac7351be12d4349
Maz <mazspm@icloud.com>
2024-04-15 19:32:05 -0500

    init

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "@/styles/globals.css";
+
+import { Lato } from "next/font/google";
+
+const lato = Lato({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  weight: ["300", "400", "700"],
+  style: ["normal", "italic"],
+});
+
+export const metadata = {
+  title: "Garo - A Gallery App",
+  description: "Garo - Japanese for Gallery.",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${lato.variable}`}>{children}</body>
+    </html>
+  );
+}
