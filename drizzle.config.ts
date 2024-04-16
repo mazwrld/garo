28c22038be11585a2b842f699e6ca3fcaf610fe5
Maz <mazspm@icloud.com>
2024-04-15 20:41:13 -0500

    configure a database, pushed current schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import { type Config } from "drizzle-kit";
 import { env } from "env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
+  schema: "./server/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: env.POSTGRES_URL,
   },
   tablesFilter: ["garo_*"],
 } satisfies Config;
