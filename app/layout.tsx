a71d9656a85e7eebff713caa1db93ffbf89f6d15
Maz <mazspm@icloud.com>
2024-04-16 15:22:20 -0500

    adds clerk and middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Nav from "@/components/Nav";
 import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { Lato } from "next/font/google";
 
@@ -22,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${lato.variable} flex flex-col gap-4`}>
-        <Nav />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${lato.variable} flex flex-col gap-4`}>
+          <Nav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
