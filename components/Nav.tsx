18638e0216d03cf1382b04a8810290c21e588838
Maz <mazspm@icloud.com>
2024-04-16 19:00:15 -0500

    on successful uploads the page now will refresh to show the new data, removes redundancy on posts on homepage

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { UploadButton } from "@/utils/uploadthing";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 export default function Nav() {
+  const router = useRouter();
   return (
     <header className="sticky z-50 flex items-center justify-center">
       {/* <nav className="supports-[backdrop-filter] absolute w-full rounded-none bg-zinc-950/50 shadow-lg backdrop-blur-[0.5rem] transition-all duration-200 sm:top-4 sm:w-[36rem] sm:rounded-full"> */}
@@ -12,7 +16,12 @@ export default function Nav() {
             <SignInButton></SignInButton>
           </SignedOut>
           <SignedIn>
-            <UploadButton endpoint="imageUploader" />
+            <UploadButton
+              endpoint="imageUploader"
+              onClientUploadComplete={() => {
+                router.refresh();
+              }}
+            />
             <UserButton></UserButton>
           </SignedIn>
         </div>
