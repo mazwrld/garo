0c671015ad7746f81b94877cb494bbfb9a675ee9
Maz <mazspm@icloud.com>
2024-04-18 10:30:55 -0500

    updated version of next, and using next image

--- components/images.tsx
+++ components/images.tsx
@@ -1,12 +1,19 @@
+import Image from 'next/image'
 import { getUserImages } from '@/server/queries'
 
 export default async function Images() {
   const images = await getUserImages()
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} />
+        <div key={image.id} className="flex w-48 h-48 flex-col">
+          <Image
+            src={image.url}
+            style={{ objectFit: 'contain' }}
+            width={480}
+            height={480}
+            alt={`The name of the file is${image.name}. This an image a user uploaded.`}
+          />
           <div>{image.name}</div>
         </div>
       ))}
