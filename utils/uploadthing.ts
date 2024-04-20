e666c63e878c2c006c7ee966a8524cf969c1c900
Maz <mazspm@icloud.com>
2024-04-20 16:41:31 -0500

    Add generateReactHelpers to uploadthing.ts

--- utils/uploadthing.ts
+++ utils/uploadthing.ts
@@ -1,9 +1,11 @@
 import {
+  generateReactHelpers,
   generateUploadButton,
   generateUploadDropzone,
 } from '@uploadthing/react'
 
-import type { uploadThingFileRouter } from '@/app/api/uploadthing/core'
+import { type uploadThingFileRouter } from '@/app/api/uploadthing/core'
 
 export const UploadButton = generateUploadButton<uploadThingFileRouter>()
 export const UploadDropzone = generateUploadDropzone<uploadThingFileRouter>()
+export const { useUploadThing } = generateReactHelpers<uploadThingFileRouter>()
