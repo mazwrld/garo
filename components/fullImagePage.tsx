08526f7d93fb01f25cff25dd509f5651d9c8a0b8
Maz <mazspm@icloud.com>
2024-04-20 15:43:42 -0500

    Refactor modal styling and fix full screen dialog

--- components/fullImagePage.tsx
+++ components/fullImagePage.tsx
@@ -1,20 +1,33 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
 import { getUserImage } from '@/server/queries'
+import { clerkClient } from '@clerk/nextjs/server'
 
 export default async function FullPageImageView(props: { imageId: number }) {
   const image = await getUserImage(props.imageId)
-
+  const uploaderInfo = await clerkClient.users.getUser(image.userId)
   return (
-    <div className="flex h-full w-full min-w-0">
-      <div className="flex flex-shrink items-center justify-center">
-        <img
-          className="flex-shrink object-contain"
+    <div className="flex h-full w-screen min-w-0">
+      <div className="relative size-full">
+        <Image
           src={image.url}
           alt={image.name}
+          fill
+          priority
+          className="object-contain"
         />
       </div>
       <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <h1 className="text-xl font-bold">{image.name}</h1>
+        <h1 className="border-b p-2 text-center text-lg">{image.name}</h1>
+        <div className="flex flex-col p-2">
+          <span className="text-zinc-400">Uploaded by:</span>
+          <span className="text-zinc-200">{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span className="text-zinc-400">Created on:</span>
+          <span className="text-zinc-200">
+            {new Date(image.createdAt).toLocaleDateString()}
+          </span>
+        </div>
       </div>
     </div>
   )
