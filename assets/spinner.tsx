9d576d78f6a057be1bcb4f8d67e858bcf8950c2c
Maz <mazspm@icloud.com>
2024-04-20 20:03:44 -0500

    spilt svg exporting react code

---
+++ assets/spinner.tsx
@@ -0,0 +1,7 @@
+export default function SpinnerSVG() {
+  return (
+    <svg className="spinner" width="24" height="24" viewBox="0 0 24 24">
+      <circle cx="12" cy="12" r="8" fill="none" />
+    </svg>
+  )
+}
--- components/costumeUploadButton.tsx
+++ components/costumeUploadButton.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import SpinnerSVG from '@/assets/spinner'
+import UploadSVG from '@/assets/uploadSVG'
 import { useUploadThing } from '@/utils/uploadthing'
 import { usePostHog } from 'posthog-js/react'
 import { toast } from 'sonner'
@@ -30,27 +32,6 @@ const useUploadThingInputProps = (...args: Input) => {
   }
 }
 
-function UploadSVG() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="h-6 w-6"
-    >
-      <path d="M11.47 1.72a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1-1.06 1.06l-1.72-1.72V7.5h-1.5V4.06L9.53 5.78a.75.75 0 0 1-1.06-1.06l3-3ZM11.25 7.5V15a.75.75 0 0 0 1.5 0V7.5h3.75a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h3.75Z" />
-    </svg>
-  )
-}
-
-function Spinner() {
-  return (
-    <svg className="spinner" width="24" height="24" viewBox="0 0 24 24">
-      <circle cx="12" cy="12" r="8" fill="none" />
-    </svg>
-  )
-}
-
 export default function CostumeUploadButton() {
   const router = useRouter()
   const postHog = usePostHog()
@@ -59,7 +40,7 @@ export default function CostumeUploadButton() {
       postHog.capture('upload_being')
       toast(
         <div className="flex items-center gap-2">
-          <Spinner />
+          <SpinnerSVG />
           <span className="font-italic">Uploading...</span>
         </div>,
         {
