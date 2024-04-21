521593d81107aed8a8e7dc311bccaa583e33a5c2
Maz <mazspm@icloud.com>
2024-04-20 21:12:48 -0500

    adds the nice button

--- components/fullImagePage.tsx
+++ components/fullImagePage.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { getUserImage } from '@/server/queries'
 import { clerkClient } from '@clerk/nextjs/server'
 
+import ShinyButton from '@/components/shinyButton'
+
 export default async function FullPageImageView(props: { imageId: number }) {
   const image = await getUserImage(props.imageId)
   const uploaderInfo = await clerkClient.users.getUser(image.userId)
@@ -18,15 +20,21 @@ export default async function FullPageImageView(props: { imageId: number }) {
       </div>
       <div className="flex w-48 flex-shrink-0 flex-col border-l">
         <h1 className="border-b p-2 text-center text-lg">{image.name}</h1>
+
         <div className="flex flex-col p-2">
           <span className="text-zinc-400">Uploaded by:</span>
           <span className="text-zinc-200">{uploaderInfo.fullName}</span>
         </div>
+
         <div className="flex flex-col p-2">
           <span className="text-zinc-400">Created on:</span>
           <span className="text-zinc-200">
             {new Date(image.createdAt).toLocaleDateString()}
           </span>
+        </div>
+
+        <div className="p-2">
+          <ShinyButton>Delete</ShinyButton>
         </div>
       </div>
     </div>
---
+++ components/shinyButton.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
+export default function ShinyButton({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <motion.button
+      initial={{ '--x': '100%', scale: 1 }}
+      animate={{ '--x': '-100%' }}
+      whileTap={{ scale: 0.88 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: 'loop',
+        repeatDelay: 1,
+        type: 'spring',
+        stiffness: 20,
+        damping: 15,
+        mass: 2,
+        scale: {
+          type: 'spring',
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
+      className="radial-gradient relative rounded-md px-6 py-2"
+    >
+      <span className="linear-mask relative block h-full w-full tracking-wide">
+        {children}
+      </span>
+      <span className="linear-overlay absolute inset-0 block rounded-md p-px" />
+    </motion.button>
+  )
+}
