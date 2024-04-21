83c09e57bf9a694cd645264c9bd13a00c7956f1d
Maz <mazspm@icloud.com>
2024-04-20 19:29:32 -0500

    adds basic posthog analytics

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 import CostumeUploadButton from '@/components/costumeUploadButton'
@@ -5,7 +6,7 @@ import CostumeUploadButton from '@/components/costumeUploadButton'
 export default function Nav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Garo</div>
+      <Link href="/">Garo</Link>
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton></SignInButton>
--- next.config.js
+++ next.config.js
@@ -18,6 +18,18 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ]
+  },
 }
 
 const config = withSentryConfig(
